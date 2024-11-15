8da4bf007add9c13866b17adf687ce2a3f1579b2
harshrbs2805 <91825855+harshrbs2805@users.noreply.github.com>
2024-11-15 14:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function firstNonRepeatedChar(str) {
+
+	let charCount={}
+	for(let char of str){
+		charCount[char]=(charCount[char]||0)+1
+	}
+
+	for(let item in charCount){
+		if(charCount[item]==1){
+			console.log(item)
+			return item
+		}
+	}
+	return null
+
+	
  // Write your code here
 }
 const input = prompt("Enter a string");
